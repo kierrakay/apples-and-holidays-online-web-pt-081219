716829a9d7b40902ec867bffb5c19da4bc1686dd
Kierra M <k.merriman32@hotmail.com>
2019-08-29 23:06:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,13 +75,26 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
- holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
-    end 
-  end.flatten.compact
-end
+ #holiday_hash.map do |season, holiday|
+ #   holiday.map do |holiday, item|
+ #     holiday if item.include?("BBQ")
+ #   end 
+ # end.flatten.compact
+#end
 
+answer = []
+holiday_hash.each do |season, data|
+  data.each do |holiday, value|
+    value.each do |supply|
+     if supply == "BBQ"
+        answer.push(holiday)
+      end
+    end
+  end
+end
+answer
+
+end
 
 
 
